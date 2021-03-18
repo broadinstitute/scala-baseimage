dfd5fb053e75ca737396ae379fdd75d77f2735c9
Isaac Zarsky <zarsky@broadinstitute.org>
2021-03-18 17:36:59 -0400

    add sbt 1.4.9 images (#5)

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Builds pair a scala version with an sbt version
-SCALA_VERSIONS=(  2.12.11   2.12.12   2.12.12   2.12.13   2.13.5)
-SBT_VERSIONS=(    1.4.7     1.4.6     1.4.7     1.4.7     1.4.7)
+SCALA_VERSIONS=(  2.12.11   2.12.12   2.12.12   2.12.12   2.12.13   2.12.13   2.13.5)
+SBT_VERSIONS=(    1.4.7     1.4.6     1.4.7     1.4.9     1.4.7     1.4.9     1.4.7)
 
 # $((x - 1)) is needed because otherwise it actually outputs length, but we need to be zero-indexed
 for i in $(seq 0 $((${#SCALA_VERSIONS[@]} - 1))); do
